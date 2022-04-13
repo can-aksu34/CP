369389cecf6eb43388d0e5a7a1a51f97c0a97c04
can-aksu34 <canaksuga@gmail.com>
2022-04-13 15:48:57 +0200

    DemarrageProjet

---
+++ PrestigeApplication/src/components/Menu.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import "../styles/style.css"
+
+function Menu() {
+    return (
+    <ul>
+  <li><a href="#">À PROPOS</a></li>
+
+  <li><a href="#">SERVICES</a></li>
+
+      
+
+  <li><a href="#">RÉALISATIONS </a></li>
+
+  <li><a href="#">CONTACT </a></li>
+</ul>
+
+       
+      );
+    }
+    
+
+export default Menu;
+
+
+  
